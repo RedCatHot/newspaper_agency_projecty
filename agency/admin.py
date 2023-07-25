0a9cd408c31c08dba0732aa538c5994db2ebe1b4
Vlad Dukh <vladdukh15@gmail.com>
2023-07-25 19:24:27 +0300

    Add models to admin panel. Add search and filter in admin panel for Newspaper model. Modified Redactor model in admin page(changed display list, fieldsets and add_fieldsets)

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from agency.models import Newspaper, Topic, Redactor
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ["title", "content", "published_date", "topic"]
+    list_filter = ["title"]
+    search_fields = ["title"]
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "years_of_experience",
+                )
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name", "last_name", "years_of_experience"
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(Topic)
