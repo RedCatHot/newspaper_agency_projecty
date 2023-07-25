ea225cdd523db6cd23da5b040ffdd8b6cbf2f42d
Vlad Dukh <vladdukh15@gmail.com>
2023-07-25 19:47:08 +0300

    Edited settings.py, also edited urls for newspaper_agency and agency

--- agency/urls.py
+++ agency/urls.py
@@ -1,9 +1,11 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from agency.views import (
+    index
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("agency.urls", namespace="agency"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("", index, name="index"),
+]
+
+app_name = "newspaper"
--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(file).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -100,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "newspaper.Redactor"
+AUTH_USER_MODEL = "agency.Redactor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
