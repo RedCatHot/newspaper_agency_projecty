cb63c4f4168112b43d31307f17b0e1f0073b5881
Vlad Dukh <vladdukh15@gmail.com>
2023-07-25 19:46:27 +0300

    Create home page for agency. Created base.html,index.html and index view

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from agency.models import Topic, Newspaper, Redactor
+
+
+def index(request):
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers,
+        "num_redactors": num_redactors,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "agency/index.html", context=context)
