27faba730f967613d4163bebf05621954644f3ef
Vlad Dukh <vladdukh15@gmail.com>
2023-09-12 13:57:36 +0300

    Solution

--- agency/models.py
+++ agency/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     class Meta:
         ordering = ["name"]
